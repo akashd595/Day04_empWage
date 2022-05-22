e0ec8708b6351f2737fcaa7971235e5162cb7772
Akash Das <akashd595@gmail.com>
2022-05-22 18:32:50 +0530

    ADDED: added in master origin

--- emplWageDay04.java
+++ emplWageDay04.java
@@ -1,5 +1,7 @@
 package com.bridgelabz.Day04_empWage;
 
 public class emplWageDay04 {
-
+    public static void main(String[] args) {
+        System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
+    }
 }
